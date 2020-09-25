d920d2b72c5c2d7af9c95964e332a41939c590c6
wjj蔚将将 <jiangjiangwei@ctrip.com>
2020-09-25 19:09:33 +0800

    ad

--- first/app/src/main/java/com/example/first/DisplayMessageActivity.java
+++ first/app/src/main/java/com/example/first/DisplayMessageActivity.java
@@ -2,8 +2,11 @@ package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -17,4 +20,26 @@ public class DisplayMessageActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         textView.setText("message: " + message);
     }
+
+    public void back(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("data", "data string");
+        setResult(123, intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("确认要关闭吗");
+        alertDialog.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DisplayMessageActivity.super.onBackPressed();
+            }
+        });
+        alertDialog.setNegativeButton("取消", null);
+        alertDialog.show();
+    }
+
 }
--- first/app/src/main/java/com/example/first/MainActivity.java
+++ first/app/src/main/java/com/example/first/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.first;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,6 +21,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("onResume");
+    }
+
+    //  没生效，待学习生命周期感知组件
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    public void resume() {
+        System.out.println("resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("onDestroy");
     }
 
     public void sendMessage(View view) {
@@ -27,6 +76,22 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        startActivityForResult(intent, 123);
+//        startActivity(intent);
+    }
+
+    public void sendMessage2(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(resultCode);
+        System.out.println(requestCode);
+        if (data != null) {
+            System.out.println(data.getStringExtra("data"));
+        }
     }
 }
